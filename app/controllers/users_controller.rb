07f968e069f7ab6af88f0fa764f5c1a5220ab74b
minhcvbachkhoa <minhcvbachkhoa@gmail.com>
2017-04-22 09:33:12 +0700

    Finish login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-  
+  def show
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:warning] = t "user_not_found"
+      redirect_to root_path
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -7,18 +14,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "hello"
       redirect_to @user
     else
       render :new
-    end
-  end
-  
-  def show
-    @user = User.find_by id: params[:id]
-    unless @user
-      flash[:warning] = t "user_not_found"
-      redirect_to root_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/sign_up", to: "users#new"
+  get "/log_in", to: "sessions#new"
   post "/sign_up", to: "users#create"
-  
+  post "/log_in", to: "sessions#create"
+  delete "/log_out", to: "sessions#destroy"
+
   resources :users, only: [:new, :show, :create]
 end
